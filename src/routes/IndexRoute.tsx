a02a7bf1dd26dd6d752bfcc82a08107ea0ddaf6a
zombierantcasey <mail@pierremackay.com>
2024-03-07 18:49:20 -0500

    load latest chat on page load

--- src/routes/IndexRoute.tsx
+++ src/routes/IndexRoute.tsx
@@ -1,12 +1,21 @@
+import { useNavigate } from "@tanstack/react-location";
 import { useLiveQuery } from "dexie-react-hooks";
+import { useEffect } from "react";
 import { db } from "../db";
 
 export function IndexRoute() {
-  const settings = useLiveQuery(() => db.settings.get("general"));
-  const { openAiApiKey } = settings ?? {};
+  const navigate = useNavigate();
+  const chats = useLiveQuery(() => db.chats.orderBy("createdAt").reverse().toArray());
+
+  useEffect(() => {
+    if (chats && chats.length > 0) {
+      navigate({ to: `/chats/${chats[0].id}` });
+    }
+  }, [chats, navigate]);
 
   return (
-    <>
-    </>
+    <div>
+      {}
+    </div>
   );
 }
